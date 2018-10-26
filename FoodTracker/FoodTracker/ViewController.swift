b47049f9a78ae72b97671b7e4b02827731b95d0a
Jason Fevang <jfevang@asb9700-e02.csil.sfu.ca>
2018-10-26 16:27:34 -0700

    Build a Basic UI Complete

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Jason Fevang on 10/26/18.
+//  Copyright © 2018 Jason Fevang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
