af3a5c4ac91701d705ffc5bd324f99b5b4e53433
Jason Fevang <jfevang@asb9700-e01.csil.sfu.ca>
2018-10-30 12:23:45 -0700

    Finished Implement a Custom Control, Define Your Data Model and started Create a Table View

---
+++ FoodTracker/FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Jason Fevang on 10/30/18.
+//  Copyright © 2018 Jason Fevang. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
